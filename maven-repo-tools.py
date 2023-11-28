ec50f0281c486cc866d86dd5012806f065bda2a9
ted <ted107127@weexglobal.com>
2023-11-28 08:30:16 +0800

    init

--- maven-repo-tools.py
+++ maven-repo-tools.py
@@ -26,10 +26,12 @@ print('walking path:', path)
 w = walk(path)
 for (dirpath, dirnames, filenames) in w:
     isleaf = False
+    onlyPom = True 
     for filename in filenames:
         if filename.endswith('.pom'):
             isleaf = True
-            break 
+        if filename.endswith('.jar'):
+            onlyPom = False
     if isleaf:
         package_path_arr = dirpath[len(args.rootpath):].replace('//', '/').split('/')
         groupId = '.'.join(package_path_arr[0:-2])
@@ -37,20 +39,26 @@ for (dirpath, dirnames, filenames) in w:
             groupId = groupId[1:]
         artifactId = package_path_arr[-2:-1][0]
         version = package_path_arr[-1:][0]
-        cmd = ['mvn', 'deploy:deploy-file', '-Durl='+target, '-DgroupId='+groupId, '-DartifactId='+artifactId, '-Dversion='+version, '-DrepositoryId='+repoId, '-DgeneratePom=false']
-        has_pom=False
-        has_file=False
-        has_sources=False
-        for filename in filenames:
-            if not has_pom and filename.endswith('.pom'):
-                cmd.append('-DpomFile='+dirpath+'/'+filename)
-                has_pom = True
-            if not has_file and filename.endswith('.jar') and not filename.endswith('-sources.jar'):
-                cmd.append('-Dfile='+dirpath+'/'+filename)
-                has_file = True
-            if not has_sources and filename.endswith('-sources.jar'):
-                cmd.append('-Dsources='+dirpath+'/'+filename)
-                has_sources = True
+        if onlyPom:
+            for filename in filenames:
+                if filename.endswith('.pom'):
+                    cmd = ['mvn', 'deploy:deploy-file', '-Durl='+target, '-DgroupId='+groupId, '-DartifactId='+artifactId, '-Dversion='+version, '-DrepositoryId='+repoId, '-Dpackaging=pom', '-Dfile='+dirpath+'/'+filename]
+                    break
+        else:
+            cmd = ['mvn', 'deploy:deploy-file', '-Durl='+target, '-DgroupId='+groupId, '-DartifactId='+artifactId, '-Dversion='+version, '-DrepositoryId='+repoId, '-DgeneratePom=false']
+            has_pom=False
+            has_file=False
+            has_sources=False
+            for filename in filenames:
+                if not has_pom and filename.endswith('.pom'):
+                    cmd.append('-DpomFile='+dirpath+'/'+filename)
+                    has_pom = True
+                if not has_file and filename.endswith('.jar') and not filename.endswith('-sources.jar'):
+                    cmd.append('-Dfile='+dirpath+'/'+filename)
+                    has_file = True
+                if not has_sources and filename.endswith('-sources.jar'):
+                    cmd.append('-Dsources='+dirpath+'/'+filename)
+                    has_sources = True
 
         print('deploying', groupId+':'+artifactId+':'+version)
         if args.verbose:
